d2eed8a380bfad6d425be3ade34ac64b947a7c5e
JoshuaERichardson <joshuarichardson1478@gmail.com>
2023-04-11 11:11:33 -0600

    finished login page.  Added loginScripts.js, login_stylesheet.css.  Redirect route on login page blocked until Auth0 functional

---
+++ public/scripts/loginScripts.js
@@ -0,0 +1,7 @@
+document.querySelector('.switch-left')
+    .addEventListener('click', (e)=> {
+        
+        setTimeout(() => {
+            window.location.replace("/login")},
+            1000)
+    })
